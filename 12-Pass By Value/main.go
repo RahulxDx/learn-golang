27f4d976b1c5bd4aeca4bb944356453f08c55ff3
Rahul R N <rahulrnsr@gmail.com>
2025-10-08 11:52:33 +0530

    Completed Module 12: Pass by Value

---
+++ 12-Pass By Value/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("=== Pass by Value in Go ===")
+
+	num := 10
+	fmt.Println("Before function call, num =", num)
+
+	double(num)
+	fmt.Println("After function call, num =", num)
+
+	str := "Hello"
+	fmt.Println("\nBefore function call, str =", str)
+
+	changeString(str)
+	fmt.Println("After function call, str =", str)
+}
+
+func double(n int) {
+	n = n * 2
+	fmt.Println("Inside function, n =", n)
+}
+
+func changeString(s string) {
+	s = s + " Go"
+	fmt.Println("Inside function, s =", s)
+}
